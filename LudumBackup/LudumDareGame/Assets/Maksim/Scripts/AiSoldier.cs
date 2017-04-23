81e3e4ad1a58b258f7ce2fa4184a84809d1e04b4
maksch <maksim.sen@hotmail.co.il>
2017-04-23 02:45:03 -0400

    Heres a backup of everything so far.

---
+++ LudumBackup/LudumDareGame/Assets/Maksim/Scripts/AiSoldier.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiSoldier : MonoBehaviour {
+    
+    string myColorType;
+    public bool isFighting;
+    Vector3 enemyLocation = new Vector3(0, 0, 0);
+    float timePassed;
+    float bulletTimeLimit = 1.9f;
+    GameObject bullet;
+    GameObject newBullet;
+    BulletMovement bulletMovement;
+    AiMovement myMovement;
+    Vector3 dirVect;
+    // Use this for initialization
+    void Start () {
+        myColorType = gameObject.tag;
+        myMovement = transform.parent.gameObject.GetComponent<AiMovement>();
+        bullet = (GameObject)Resources.Load("Bullet");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isFighting)
+        {
+            timePassed += Time.deltaTime;
+            if(timePassed > bulletTimeLimit)
+            {
+                dirVect = (enemyLocation - transform.position).normalized;
+                newBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+                bulletMovement = newBullet.GetComponent<BulletMovement>();
+                bulletMovement.xMove = Random.Range(dirVect.x - 0.001f, dirVect.x + 0.001f) * 0.01f;
+                bulletMovement.yMove = Random.Range(dirVect.y - 0.001f, dirVect.y + 0.001f) * 0.01f;
+                bulletMovement.shooterId = transform.parent.gameObject.GetInstanceID();
+                timePassed = 0;
+                isFighting = false;
+                myMovement.allowMovement = true;
+            }
+        }
+	}
+    void OnTriggerEnter(Collider c)
+    {
+        if (myColorType != c.gameObject.tag)
+        {
+            isFighting = true;
+            //Vector3 difference = c.gameObject.transform.position - transform.parent.transform.position;
+            //float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            //transform.parent.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+            enemyLocation = c.gameObject.transform.position;
+            myMovement.allowMovement = false;
+        }
+    }
+    void OnTriggerStay(Collider c)
+    {
+        if (myColorType != c.gameObject.tag)
+        {
+            isFighting = true;
+            //Vector3 difference = c.gameObject.transform.position - transform.parent.transform.position;
+            //float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            //transform.parent.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+            enemyLocation = c.gameObject.transform.position;
+            myMovement.allowMovement = false;
+        }
+    }
+    void OnTriggerExit(Collider c)
+    {
+        if (myColorType != c.gameObject.tag)
+        {
+            isFighting = false;
+            myMovement.allowMovement = true;
+        }
+    }
+}
---
+++ LudumBackup/LudumDareGame/Assets/Maksim/Scripts/SoldierStats.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+public class SoldierStats : MonoBehaviour
+{
+    public float AiHealth = 100f;
+
+    Transform healthBar;
+    float initXScale;
+    void Start()
+    {
+        healthBar = transform.parent.Find("Health");
+        initXScale = healthBar.localScale.x;
+    }
+    public void ApplyDamage(float Bdmg)
+    {
+        AiHealth -= Bdmg;
+        healthBar.localScale = new Vector3(initXScale * (AiHealth / 100f), healthBar.localScale.y, healthBar.localScale.z);
+    }
+    bool isDead() //returns if soldier is dead
+    {
+        if (AiHealth <= 0)
+        {
+            //Destroy(gameObject);
+            return true;
+        }
+        else
+            return false;
+    }
+    void Update()
+    {
+        if (isDead() == true) { Destroy(gameObject.transform.parent.gameObject); }
+    }
+}
