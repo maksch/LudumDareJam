daccae9e9b8ed047df90d6be6c2f02241b2505c7
jesse3339 <jesse3339@yahoo.com>
2017-04-23 05:17:24 -0400

    game cond + Team Counter

---
+++ LudumBackup/LudumDareGame/Assets/Maksim/Scripts/GameConditions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+public class GameConditions : MonoBehaviour
+{
+    TeamCounter PC; //Player Count
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (PC.totalRed <= 0)
+        {
+            //ALL OF RED HAS DIED
+            //Return to Main Menu
+        }
+        if (PC.totalBlue <= 0)
+        {
+            //ALL OF BLUE HAS DIED
+            //Return to Main Menu
+        }
+        if (PC.totalGreen <= 0)
+        {
+            //ALL OF GREEN HAS DIED
+            //Return to Main Menu
+        }
+    }
+    
+}
+
+
---
+++ LudumBackup/LudumDareGame/Assets/Maksim/Scripts/TeamCounter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+public class TeamCounter : MonoBehaviour
+{
+    public int totalRed;
+    public int totalBlue;
+    public int totalGreen;
+
+    float timePassed;
+    void Start()
+    {
+        totalRed = GameObject.FindGameObjectsWithTag("RedPlayer").Length;
+        totalBlue = GameObject.FindGameObjectsWithTag("BluePlayer").Length;
+        totalGreen = GameObject.FindGameObjectsWithTag("GreenPlayer").Length;
+    }
+
+    public void Update()
+    {
+        timePassed += Time.deltaTime;
+        if (timePassed > 2)
+        {
+            timePassed = 0;
+
+            totalRed = GameObject.FindGameObjectsWithTag("RedPlayer").Length;
+            totalBlue = GameObject.FindGameObjectsWithTag("BluePlayer").Length;
+            totalGreen = GameObject.FindGameObjectsWithTag("GreenPlayer").Length;
+            Debug.Log(totalRed);
+        }
+
+    }
+}
